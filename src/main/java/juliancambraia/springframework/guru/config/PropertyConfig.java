9fffee4edb2156f81e16fa9efb879105d374b912
JulianCambraia <juliancambraia@gmail.com>
2020-08-23 17:29:48 -0300

    Passando a usar o application.properties default do Spring Bootno PropertyConfig.

--- src/main/java/juliancambraia/springframework/guru/config/PropertyConfig.java
+++ src/main/java/juliancambraia/springframework/guru/config/PropertyConfig.java
@@ -1,29 +1,19 @@
 package juliancambraia.springframework.guru.config;
 
 import juliancambraia.springframework.examplebeans.FakeDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 /**
+ * Applicando o padrão do Application Properties
+ *
  * @author juliancambraia
  */
 
-@Configuration
-// @PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
-public class PropertyConfig {
 
-    @Autowired
-    Environment env;
+@Configuration
+public class PropertyConfig {
 
     @Value("${guru.username}")
     String userName;
@@ -56,18 +46,11 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(env.getProperty("USERNAME"));
+        fakeDataSource.setUsername(userName);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
 
         return fakeDataSource;
     }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
-    }
-
 
 }
